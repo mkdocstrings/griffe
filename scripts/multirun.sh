8bf184cd5efc0f071fdb040dac941559b33353a9
Timothée Mazzucotelli <pawamoy@pm.me>
2021-09-15 19:36:08 +0200

    chore: Test on 3.8, 3.9, 3.10, 3.11

--- scripts/multirun.sh
+++ scripts/multirun.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-PYTHON_VERSIONS="${PYTHON_VERSIONS-3.8 3.9}"
+PYTHON_VERSIONS="${PYTHON_VERSIONS-3.8 3.9 3.10 3.11}"
 
 if [ -n "${PYTHON_VERSIONS}" ]; then
     for python_version in ${PYTHON_VERSIONS}; do
-        if pdm use -f "${python_version}" &>/dev/null; then
+        if pdm use -f "python${python_version}" &>/dev/null; then
             echo "> pdm run $@ (Python ${python_version})"
             pdm run "$@"
         else
