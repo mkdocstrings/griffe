b85977646dfade4d43b41a3e68b2ad4a314d52e6
Timothée Mazzucotelli <pawamoy@pm.me>
2021-09-15 19:57:36 +0200

    tests: Fix CLI test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,7 @@ from griffe import cli
 
 def test_main():
     """Basic CLI test."""
-    assert cli.main(["griffe"]) == 0
+    assert cli.main(["griffe", "-s", "src"]) == 0
 
 
 def test_show_help(capsys):
