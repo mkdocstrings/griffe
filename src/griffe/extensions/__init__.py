db9a7b12c9a6a33b65c092eb8be5bac25e444909
Timothée Mazzucotelli <pawamoy@pm.me>
2021-09-15 19:30:25 +0200

    refactor: Expose When through extensions

--- src/griffe/extensions/__init__.py
+++ src/griffe/extensions/__init__.py
@@ -1,5 +1,5 @@
 """This module is the public interface to import elements from the base."""
 
-from griffe.extensions.base import Extension, Extensions
+from griffe.extensions.base import Extension, Extensions, When
 
-__all__ = ["Extensions", "Extension"]  # noqa: WPS410
+__all__ = ["Extensions", "Extension", "When"]  # noqa: WPS410
