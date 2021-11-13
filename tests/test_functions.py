2db531c9d0196acce7f001bf071c4bc9e7e43db1
Timothée Mazzucotelli <pawamoy@pm.me>
2021-11-13 14:38:23 +0100

    tests: Update parameters/annotations tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,6 @@
 """Test functions loading."""
 
-import inspect
-
+from griffe.dataclasses import ParameterKind
 from griffe.loader import GriffeLoader
 from tests import FIXTURES_DIR
 
@@ -19,7 +18,7 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default is None
 
     function = module["f_posonly_default"]
@@ -27,7 +26,7 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default == "0"
 
     function = module["f_posonly_poskw"]
@@ -35,12 +34,12 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default is None
     param = function.parameters[1]
     assert param is function.parameters["poskw"]
     assert param.name == "poskw"
-    assert param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+    assert param.kind is ParameterKind.positional_or_keyword
     assert param.default is None
 
     function = module["f_posonly_poskw_default"]
@@ -48,12 +47,12 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default is None
     param = function.parameters[1]
     assert param is function.parameters["poskw"]
     assert param.name == "poskw"
-    assert param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+    assert param.kind is ParameterKind.positional_or_keyword
     assert param.default == "0"
 
     function = module["f_posonly_default_poskw_default"]
@@ -61,12 +60,12 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default == "0"
     param = function.parameters[1]
     assert param is function.parameters["poskw"]
     assert param.name == "poskw"
-    assert param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+    assert param.kind is ParameterKind.positional_or_keyword
     assert param.default == "1"
 
     function = module["f_posonly_poskw_kwonly"]
@@ -74,17 +73,17 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default is None
     param = function.parameters[1]
     assert param is function.parameters["poskw"]
     assert param.name == "poskw"
-    assert param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+    assert param.kind is ParameterKind.positional_or_keyword
     assert param.default is None
     param = function.parameters[2]
     assert param is function.parameters["kwonly"]
     assert param.name == "kwonly"
-    assert param.kind is inspect.Parameter.KEYWORD_ONLY
+    assert param.kind is ParameterKind.keyword_only
     assert param.default is None
 
     function = module["f_posonly_poskw_kwonly_default"]
@@ -92,17 +91,17 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default is None
     param = function.parameters[1]
     assert param is function.parameters["poskw"]
     assert param.name == "poskw"
-    assert param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+    assert param.kind is ParameterKind.positional_or_keyword
     assert param.default is None
     param = function.parameters[2]
     assert param is function.parameters["kwonly"]
     assert param.name == "kwonly"
-    assert param.kind is inspect.Parameter.KEYWORD_ONLY
+    assert param.kind is ParameterKind.keyword_only
     assert param.default == "0"
 
     function = module["f_posonly_poskw_default_kwonly_default"]
@@ -110,54 +109,58 @@ def test_loading_functions_parameters():  # noqa: WPS218
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default is None
     param = function.parameters[1]
     assert param is function.parameters["poskw"]
     assert param.name == "poskw"
-    assert param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+    assert param.kind is ParameterKind.positional_or_keyword
     assert param.default == "0"
     param = function.parameters[2]
     assert param is function.parameters["kwonly"]
     assert param.name == "kwonly"
-    assert param.kind is inspect.Parameter.KEYWORD_ONLY
+    assert param.kind is ParameterKind.keyword_only
     assert param.default == "1"
 
     function = module["f_posonly_default_poskw_default_kwonly_default"]
     param = function.parameters[0]
     assert param is function.parameters["posonly"]
     assert param.name == "posonly"
-    assert param.kind is inspect.Parameter.POSITIONAL_ONLY
+    assert param.kind is ParameterKind.positional_only
     assert param.default == "0"
     param = function.parameters[1]
     assert param is function.parameters["poskw"]
     assert param.name == "poskw"
-    assert param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+    assert param.kind is ParameterKind.positional_or_keyword
     assert param.default == "1"
     param = function.parameters[2]
     assert param is function.parameters["kwonly"]
     assert param.name == "kwonly"
-    assert param.kind is inspect.Parameter.KEYWORD_ONLY
+    assert param.kind is ParameterKind.keyword_only
     assert param.default == "2"
 
     function = module["f_var"]
     assert len(function.parameters) == 3
     param = function.parameters[0]
     assert param.name == "*args"
-    assert param.annotation == "str"
+    assert param.annotation.source == "str"
+    assert param.annotation.full == "str"
     param = function.parameters[1]
     assert param.annotation is None
     param = function.parameters[2]
     assert param.name == "**kwargs"
-    assert param.annotation == "int"
+    assert param.annotation.source == "int"
+    assert param.annotation.full == "int"
 
     function = module["f_annorations"]
     assert len(function.parameters) == 4
     param = function.parameters[0]
-    assert param.annotation == "str"
+    assert param.annotation.source == "str"
+    assert param.annotation.full == "str"
     param = function.parameters[1]
-    assert param.annotation == "Any"
+    assert param.annotation.source == "Any"
+    assert param.annotation.full == "typing.Any"
     param = function.parameters[2]
-    assert param.annotation == "typing.Optional[typing.List[int]]"
+    assert str(param.annotation) == "typing.Optional[typing.List[int]]"
     param = function.parameters[3]
-    assert param.annotation == "float | None"
+    assert str(param.annotation) == "float | None"
