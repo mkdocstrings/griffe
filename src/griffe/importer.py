d00721971c7b820e16e463408f04cc3e81a14db6
Timothée Mazzucotelli <pawamoy@pm.me>
2022-01-27 00:02:40 +0100

    fix: Make importer actually able to import any nested object

--- src/griffe/importer.py
+++ src/griffe/importer.py
@@ -1,9 +1,12 @@
 """This module contains utilities to dynamically import objects."""
 
+from __future__ import annotations
+
+from importlib import import_module
 from typing import Any
 
 
-def dynamic_import(import_path: str) -> Any:
+def dynamic_import(import_path: str) -> Any:  # noqa: WPS231
     """Dynamically import the specified object.
 
     It can be a module, class, method, function, attribute,
@@ -13,17 +16,26 @@ def dynamic_import(import_path: str) -> Any:
         import_path: The path of the object to import.
 
     Raises:
-        ImportError: When there was an error during import.
+        ModuleNotFoundError: When the object's module could not be found.
 
     Returns:
         The imported object.
     """
-    try:
-        module = __import__(import_path, level=0)
-    except Exception as error:
-        raise ImportError(f"Error while importing '{import_path}': {error}") from error
-    attr_parts = import_path.split(".")[1:]
+    module_parts: list[str] = import_path.split(".")
+    object_parts: list[str] = []
+
+    while True:
+        module_path = ".".join(module_parts)
+        try:  # noqa: WPS503 (false-positive)
+            module = import_module(module_path)
+        except ModuleNotFoundError:
+            if len(module_parts) == 1:
+                raise
+            object_parts.insert(0, module_parts.pop(-1))
+        else:
+            break
+
     value = module
-    for part in attr_parts:
+    for part in object_parts:
         value = getattr(value, part)
     return value
