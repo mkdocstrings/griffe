dfcf749992537ad037a6570299b74c3790407f11
Timothée Mazzucotelli <pawamoy@pm.me>
2022-01-27 19:01:35 +0100

    chore: Clean up `fixsetup` script

--- scripts/fixsetup.sh
+++ scripts/fixsetup.sh
@@ -5,10 +5,6 @@ PYTHON_VERSIONS="${PYTHON_VERSIONS-3.7 3.8 3.9 3.10 3.11}"
 
 for python_version in ${PYTHON_VERSIONS}; do
     rm -rf "__pypackages__/${python_version}/lib/mkdocstrings"
-    rm -f "__pypackages__/${python_version}/lib/mkdocstrings.pth"
-    rm -rf "__pypackages__/${python_version}/lib/mkdocs_autorefs"
-    rm -f "__pypackages__/${python_version}/lib/mkdocs_autorefs.pth"
     cp -r ../mkdocstrings/src/mkdocstrings "__pypackages__/${python_version}/lib/"
-    cp -r ../mkdocstrings-python/src/mkdocstrings/* "__pypackages__/${python_version}/lib/mkdocstrings"
-    cp -r ../mkdocs-autorefs/src/mkdocs_autorefs "__pypackages__/${python_version}/lib/"
+    cp -r ../mkdocstrings-python/src/mkdocstrings_handlers "__pypackages__/${python_version}/lib/"
 done
