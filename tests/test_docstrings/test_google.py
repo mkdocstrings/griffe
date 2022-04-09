374dec5efe17c28a5d8c7b9b8035c15cc61f7084
KevinMusgrave <tkm45@cornell.edu>
2022-04-08 20:06:10 -0400

    Add missing word in assert

--- tests/test_docstrings/test_google.py
+++ tests/test_docstrings/test_google.py
@@ -726,7 +726,7 @@ def test_extra_parameter(parse_google):
     )
     assert len(sections) == 1
     assert len(warnings) == 3
-    assert "'x' does not appear in parent signature" in warnings[1]
+    assert "'x' does not appear in the parent signature" in warnings[1]
 
 
 # TODO: possible feature
