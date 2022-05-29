bae435887a563b89adea3ee5b220a90ad7654686
Talley Lambert <talley.lambert@gmail.com>
2022-05-29 12:10:23 -0400

    test: combine tests

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -1,11 +1,7 @@
 """Tests for the `encoders` module."""
-
-import json
-
 import pytest
 
 from griffe.dataclasses import Function, Module, Object
-from griffe.encoders import JSONEncoder, json_decoder
 from griffe.loader import GriffeLoader
 
 
@@ -18,22 +14,11 @@ def test_minimal_data_is_enough():
     """
     loader = GriffeLoader()
     module = loader.load_module("griffe")
-    minimal = json.dumps(module, cls=JSONEncoder, full=False, indent=2)
-    full = json.dumps(module, cls=JSONEncoder, full=True, indent=2)
-    reloaded = json.loads(minimal, object_hook=json_decoder)
-    assert json.dumps(reloaded, cls=JSONEncoder, full=False, indent=2) == minimal
-    assert json.dumps(reloaded, cls=JSONEncoder, full=True, indent=2) == full
-
-
-def test_object_as_from_json():
-    """Test serialization and de-serialization convenience methods on Object."""
-    loader = GriffeLoader()
-    module = loader.load_module("griffe")
     minimal = module.as_json(full=False, indent=2)
-    assert minimal == json.dumps(module, cls=JSONEncoder, full=False, indent=2)
+    full = module.as_json(full=True, indent=2)
     reloaded = Module.from_json(minimal)
-    # recasting to json for sake of comparison easier than as_dict()
-    assert module.as_json() == reloaded.as_json()
+    assert reloaded.as_json(full=False, indent=2) == minimal
+    assert reloaded.as_json(full=True, indent=2) == full
 
     # also works (but will result in a different type hint)
     assert Object.from_json(minimal)
