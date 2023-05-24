29529f1a1fa1e0418437a66de00bfed11d9ecc86
Timothée Mazzucotelli <pawamoy@pm.me>
2023-05-24 10:20:17 +0200

    fixup! fix: full method over expressions of expressions

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -42,13 +42,17 @@ def test_explode_return_annotations(annotation: str, items: int) -> None:
         assert sections[1].value
 
 
-@pytest.mark.parametrize("annotation", [
-    "int",
-    "tuple[int]",
-    "dict[str, str]",
-    "Optional[tuple[int, float]]",
-])
-def test_full(annotation):
+@pytest.mark.parametrize(
+    "annotation",
+    [
+        "int",
+        "tuple[int]",
+        "dict[str, str]",
+        "Optional[tuple[int, float]]",
+    ],
+)
+def test_full(annotation: str) -> None:
+    """Assert we can transform expressions to their full form."""
     code = f"x: {annotation}"
     with temporary_visited_module(code) as module:
         obj = module["x"]
