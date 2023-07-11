6d9d6b06bd985c509c4aa4c82369bbfa93162472
Timothée Mazzucotelli <pawamoy@pm.me>
2023-07-11 15:47:12 +0200

    chore: Update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -2,13 +2,13 @@
 
 repo=$(realpath $(dirname "$0"))
 tags=(
-    0.25.5
-    0.26.0
-    0.27.5
-    0.28.2
-    0.29.1
-    0.30.1
-    0.31.0
+    # 0.25.5
+    # 0.26.0
+    # 0.27.5
+    # 0.28.2
+    # 0.29.1
+    # 0.30.1
+    # 0.31.0
     HEAD
 )
 echo "Testing tags: ${tags[@]}"
@@ -26,6 +26,7 @@ for tag in ${tags[@]}; do
             else
                 venv${tag}/bin/python -m pip install griffe==${tag} &>/dev/null
             fi
+            venv${tag}/bin/python -m pip install pyinstrument scalene memray &>/dev/null
         fi
     ) &
 done
@@ -33,21 +34,49 @@ wait
 
 cat <<EOF >benchmark.py
 import sys
+import griffe
 from griffe.loader import GriffeLoader
-stblib_packages = sorted([m for m in sys.stdlib_module_names if not m.startswith("_")])
-loader = GriffeLoader(allow_inspection=False)
-for package in stblib_packages:
+from griffe.extensions import load_extensions
+from griffe.extensions import Extension
+stdlib_packages = sorted([m for m in sys.stdlib_module_names if not m.startswith("_")])
+# extensions = load_extensions([
+#     Extension, Extension, Extension, Extension,
+#     Extension, Extension, Extension, Extension,
+#     Extension, Extension, Extension, Extension,
+#     Extension, Extension, Extension, Extension,
+# ])
+extensions = None
+loader = GriffeLoader(allow_inspection=False, extensions=extensions)
+for package in stdlib_packages:
     try:
         loader.load_module(package)
-    except Exception:
+    except:
         pass
 loader.resolve_aliases(implicit=False, external=False)
 EOF
 
-commands=$(
-    for tag in ${tags[@]}; do
-        echo "'venv${tag}/bin/python benchmark.py'"
-    done
-)
+if [ "$1" = "hyperfine" ]; then
+    commands=$(
+        for tag in ${tags[@]}; do
+            echo "'venv${tag}/bin/python benchmark.py'"
+        done
+    )
 
-eval hyperfine --runs 3 ${commands} --export-json benchmark.json
+    eval hyperfine --show-output --runs 2 ${commands} --export-json benchmark.json
+elif [ "$1" = "scalene" ]; then
+    for tag in ${tags[@]}; do
+        venv${tag}/bin/python -m scalene --cli --cpu --memory --profile-all benchmark.py
+    done
+elif [ "$1" = "memray" ]; then
+    for tag in ${tags[@]}; do
+        venv${tag}/bin/python -m memray run -o report${tag}.bin benchmark.py
+        # venv${tag}/bin/python -m memray flamegraph report${tag}.bin
+        # venv${tag}/bin/python -m memray tree report${tag}.bin
+        # venv${tag}/bin/python -m memray summary report${tag}.bin
+        venv${tag}/bin/python -m memray stats report${tag}.bin
+    done
+elif [ "$1" = "pyinstrument" ]; then
+    for tag in ${tags[@]}; do
+        venv${tag}/bin/pyinstrument benchmark.py
+    done
+fi
