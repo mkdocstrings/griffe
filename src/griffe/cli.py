0493f05a2f1b47e0362a6a689eae3c77586faa32
Timothée Mazzucotelli <pawamoy@pm.me>
2023-08-02 16:43:59 +0200

    fixup! feat: Add `--version` and `-V` cli flag

--- src/griffe/cli.py
+++ src/griffe/cli.py
@@ -19,7 +19,8 @@ import logging
 import os
 import sys
 from datetime import datetime, timezone
-from importlib.metadata import version as get_version
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as get_dist_version
 from pathlib import Path
 from typing import IO, TYPE_CHECKING, Any, Callable, Sequence
 
@@ -35,14 +36,19 @@ from griffe.loader import GriffeLoader, load
 from griffe.logger import get_logger
 from griffe.stats import _format_stats
 
-version = get_version("griffe")
-
 if TYPE_CHECKING:
     from griffe.extensions import Extensions, ExtensionType
 
 
 DEFAULT_LOG_LEVEL = os.getenv("GRIFFE_LOG_LEVEL", "INFO").upper()
 logger = get_logger(__name__)
+
+
+def _get_griffe_version() -> str:
+    try:
+        return get_dist_version("griffe")
+    except PackageNotFoundError:
+        return "0.0.0"
 
 
 def _print_data(data: str, output_file: str | IO | None) -> None:
@@ -125,7 +131,7 @@ def get_parser() -> argparse.ArgumentParser:
 
     global_options = parser.add_argument_group(title="Global options")
     global_options.add_argument("-h", "--help", action="help", help=main_help)
-    global_options.add_argument("-V", "--version", action="version", version="%(prog)s " + version)
+    global_options.add_argument("-V", "--version", action="version", version="%(prog)s " + _get_griffe_version())
 
     def add_common_options(subparser: argparse.ArgumentParser) -> None:
         common_options = subparser.add_argument_group(title="Common options")
