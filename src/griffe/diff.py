dfffa4b96a8a70f93b899bd41aefeaa9939819e9
Geethakrishna-Puligundla <pgeethakrishna@gmail.com>
2023-11-11 11:58:44 -0500

    fix: Report attributes who lost their value as "unset"
    Issue #218: https://github.com/mkdocstrings/griffe/issues/218
    PR #225: https://github.com/mkdocstrings/griffe/pull/225

--- src/griffe/diff.py
+++ src/griffe/diff.py
@@ -415,7 +415,10 @@ def _attribute_incompatibilities(old_attribute: Attribute, new_attribute: Attrib
     # if old_attribute.annotation is not None and new_attribute.annotation is not None:
     #     if not is_subhint(new_attribute.annotation, old_attribute.annotation):
     if old_attribute.value != new_attribute.value:
-        yield AttributeChangedValueBreakage(new_attribute, old_attribute.value, new_attribute.value)
+        if new_attribute.value is None:
+            yield AttributeChangedValueBreakage(new_attribute, old_attribute.value, "unset")
+        else:
+            yield AttributeChangedValueBreakage(new_attribute, old_attribute.value, new_attribute.value)
 
 
 def _alias_incompatibilities(
