d604282932641e3e8285da83403131ba984006c9
Timothée Mazzucotelli <pawamoy@pm.me>
2024-01-15 23:18:21 +0100

    chore: Template upgrade
    Also prepare Insiders.

---
+++ docs/insiders/changelog.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## griffe Insiders
+
+### 1.0.0 <small>April 22, 2023</small> { id="1.0.0" }
+
+- Release first Insiders version
--- duties.py
+++ duties.py
@@ -197,7 +197,19 @@ def docs_deploy(ctx: Context) -> None:
     with material_insiders() as insiders:
         if not insiders:
             ctx.run(lambda: False, title="Not deploying docs without Material for MkDocs Insiders!")
-        ctx.run(mkdocs.gh_deploy(), title="Deploying documentation")
+        origin = ctx.run("git config --get remote.origin.url", silent=True)
+        if "pawamoy-insiders/griffe" in origin:
+            ctx.run("git remote add upstream git@github.com:mkdocstrings/griffe", silent=True, nofail=True)
+            ctx.run(
+                mkdocs.gh_deploy(remote_name="upstream", force=True),
+                title="Deploying documentation",
+            )
+        else:
+            ctx.run(
+                lambda: False,
+                title="Not deploying docs from public repository (do that from insiders instead!)",
+                nofail=True,
+            )
 
 
 @duty
@@ -222,6 +234,12 @@ def release(ctx: Context, version: str) -> None:
         ctx: The context instance (passed automatically).
         version: The new version number to use.
     """
+    origin = ctx.run("git config --get remote.origin.url", silent=True)
+    if "pawamoy-insiders/griffe" in origin:
+        ctx.run(
+            lambda: False,
+            title="Not releasing from insiders repository (do that from public repo instead!)",
+        )
     ctx.run("git add pyproject.toml CHANGELOG.md", title="Staging files", pty=PTY)
     ctx.run(["git", "commit", "-m", f"chore: Prepare release {version}"], title="Committing changes", pty=PTY)
     ctx.run(f"git tag {version}", title="Tagging commit", pty=PTY)
