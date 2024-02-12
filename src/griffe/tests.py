2359a02aef3cdc1776608a9e507c0096285e1d75
Timothée Mazzucotelli <pawamoy@pm.me>
2024-02-12 09:47:04 +0100

    tests: Fix macos tmp path checks

--- src/griffe/tests.py
+++ src/griffe/tests.py
@@ -35,6 +35,10 @@ class TmpPackage:
     path: Path
     """The package path."""
 
+    def __post_init__(self) -> None:
+        self.tmpdir = self.tmpdir.resolve()
+        self.path = self.path.resolve()
+
 
 @contextmanager
 def temporary_pyfile(code: str, *, module_name: str = "module") -> Iterator[tuple[str, Path]]:
