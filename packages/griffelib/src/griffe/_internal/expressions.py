c38ae0330f50a86463818a77399cdc62995c6b88
Timothée Mazzucotelli <dev@pawamoy.fr>
2026-06-23 19:22:05 +0200

    style: Format

--- packages/griffelib/src/griffe/_internal/expressions.py
+++ packages/griffelib/src/griffe/_internal/expressions.py
@@ -452,7 +452,10 @@ class ExprDict(Expr):
     def iterate(self, *, flat: bool = True) -> Iterator[str | Expr]:
         yield "{"
         yield from _join(
-            (("**", value) if key is None else (key, ": ", value) for key, value in zip(self.keys, self.values, strict=False)),
+            (
+                ("**", value) if key is None else (key, ": ", value)
+                for key, value in zip(self.keys, self.values, strict=False)
+            ),
             ", ",
             flat=flat,
         )
